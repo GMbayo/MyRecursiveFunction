7941e8f996e707cf2827c9e946f354489caf3966
MBAYO Guilain <Guilain.MBAYO@cpnv.ch>
2019-02-21 11:07:57 +0100

    Add choice quit/continue

--- main.c
+++ main.c
@@ -4,16 +4,26 @@
 int main()
 {
     int nb = 0, result = 1;
-
-    printf("Entrez un nombre entier: ");
-    scanf("%i", &nb);
-
-    for(int i = nb; i>0; i--)
+    char quit = ' ';
+    do
     {
-        result= result * i;
-    }
+        fflush(stdin);
+        printf("Entrez un nombre entier: ");
+        scanf("%i", &nb);
 
-    printf("La factorielle de %i est %i", nb, result);
+        for(int i = nb; i>0; i--)
+        {
+            result= result * i;
+        }
+
+        printf("La factorielle de %i est %i\n\n", nb, result);
+        fflush(stdin);
+        printf("pour quitter, cliquez sur q, sinon, cliquez sur c: ");
+        scanf("%c", &quit);
+        system("cls");
+        result = 1;
+        nb = 0;
+    }while(quit!='q' && quit!='Q');
 
     return EXIT_SUCCESS;
 }
